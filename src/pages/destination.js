6ada49b8393f6ab2e0b541f963f6c00363f04645
Jeff <jeffgichuki68@gmail.com>
2023-05-05 00:47:11 +0300

    started

--- src/pages/destination.js
+++ src/pages/destination.js
@@ -7,8 +7,16 @@ const Destination = () => {
       <Head>
         <title>Space Tourism | Destination</title>
       </Head>
-      <section>
-        <h1>Destination</h1>
+      <section id={styles.dest_section}>
+        <div id={styles.image_col}  className={styles.dest_col}>
+          <div className={styles.img_container}>
+            <span><h1>01</h1><h2>PICK YOUR DESTINATION</h2></span>
+            <img src="./image-mars.png" />
+          </div>
+        </div>
+        <div id={styles.info_col}  className={styles.dest_col}>
+
+        </div>
       </section>
     </>
   );
